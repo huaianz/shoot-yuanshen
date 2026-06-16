b34df1510d2222cf833ffa30f40a2ca5d9f78ddd
huaianz <1462317454@qq.com>
2026-06-16 17:16:45 +0800

    修改一下非代码资源

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : SingleMonoBase<PlayerController>
+public class PlayerModel : MonoBehaviour
 {
-    //当前所操控的角色模型
-    public PlayerModel currentPlayerModel;
+    // Start is called before the first frame update
     void Start()
     {
         
